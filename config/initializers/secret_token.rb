f8fd0c9c3221ce6549149cea8f2f23c7a5dd8303
Wisam Al Abed <electron.muon.tau.neutrino@gmail.com>
2013-06-17 11:10:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EZNotes::Application.config.secret_token = 'e29ff6f9ef0397293ef4fd9b3c1f6350a32c5e210dbaa1c1cc8713aec3854ba71134ac0b37add84c22c24ef5c3719e503456fdc632d6df28324e7fa196b4da68'
