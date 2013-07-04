5e5e3dee46ffe6b01c8c19f4842b8a7613879789
Wisam Al Abed <electron.muon.tau.neutrino@gmail.com>
2013-07-04 06:00:10 -0400

    Update files for migrating to rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 EZNotes::Application.config.secret_token = 'e29ff6f9ef0397293ef4fd9b3c1f6350a32c5e210dbaa1c1cc8713aec3854ba71134ac0b37add84c22c24ef5c3719e503456fdc632d6df28324e7fa196b4da68'
+EZNotes::Application.config.secret_key_base = '569b831224688557872ac1ec4cb051e9e406e28e59e530e7051138ad765d7837f8c4b69cea85442f028ee5ffb23e7f112c75512f9e93f0569aab441f0d0f4519'
